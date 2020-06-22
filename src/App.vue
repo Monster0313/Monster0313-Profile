2c49f5520a9486bc977cf7ae46b5405d0ba17658
monster0313 <y7793800@gmail.com>
2020-06-23 02:08:51 +0800

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,25 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
     <router-view />
   </div>
 </template>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+*,
+*:before,
+*:after {
+  box-sizing: border-box;
+  margin: 0;
 }
 
-#nav {
-  padding: 30px;
+html {
+  font-family: sans-serif;
+  font-size: 16px;
+  word-spacing: 1px;
+  box-sizing: border-box;
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
+  body {
+    background-image: linear-gradient(20deg, #02c39a, #028090, #05668d);
   }
 }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,56 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div class="container">
+    <div class="chat">
+      <div class="title">
+        <h1>Monster0313</h1>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
 export default {
-  name: "Home",
-  components: {
-    HelloWorld
-  }
+  name: "home"
 };
 </script>
+
+<style lang="scss">
+$scrollbar-color-front: rgba(
+  $color: #414141,
+  $alpha: 0.2
+);
+$scrollbar-color-back: rgba(
+  $color: #f5fafa,
+  $alpha: 0
+);
+
+.container {
+  margin: 0 auto;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+  .chat {
+    width: 500px;
+    height: 80vh;
+    overflow-y: scroll;
+    scrollbar-color: $scrollbar-color-front $scrollbar-color-back;
+    scrollbar-width: thin;
+    background-color: rgba($color: #ffffff, $alpha: 0.1);
+    .title {
+    }
+  }
+}
+
+*::-webkit-scrollbar {
+  width: 6px;
+}
+*::-webkit-scrollbar-track {
+  background: $scrollbar-color-back;
+}
+*::-webkit-scrollbar-thumb {
+  background-color: $scrollbar-color-front;
+  border-radius: 20px;
+}
+</style>
